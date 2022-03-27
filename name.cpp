d96734488cdb98b4ecf56ba934b8c142ab22fd55
iceman404 <69120910+iceman404@users.noreply.github.com>
2022-03-27 09:39:55 +0545

    Update name.cpp
    I added a line.

--- name.cpp
+++ name.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main(){
 	enum color{red=0xffff, yellow, green, blue, violet};
 	cout << red <<", "<< yellow <<", "<<green<<", "<<blue<<", "<<violet<<endl;
+	cout<<"This is another commit\n";
 	return 0;
 }
